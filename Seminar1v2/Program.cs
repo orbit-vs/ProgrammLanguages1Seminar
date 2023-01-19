bb4f8cbb86f7749efc14951ae5584455a042c428
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-19 15:35:42 +0300

    Проверка квадрата числа

--- Seminar1v2/Program.cs
+++ Seminar1v2/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Проверка квадрата числа");
+Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+Console.WriteLine ("Да");
+}
+else
+{
+Console.WriteLine ("Нет");
+}
