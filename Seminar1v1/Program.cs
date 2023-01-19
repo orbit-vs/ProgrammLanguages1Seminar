385dd2efa6020db9bc9039d66cbc136023680f41
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-19 14:51:41 +0300

    квадрат числа

--- Seminar1v1/Program.cs
+++ Seminar1v1/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("Возведение числа в квадрат");
+Console.WriteLine("Пожалуйста введите число и мы возведем его в квадрат");
+
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число " + number);
+
+int result = number * number;
+Console.WriteLine("Квадрат числа " + number + " есть число " + result);
