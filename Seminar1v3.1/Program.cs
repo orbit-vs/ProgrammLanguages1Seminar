1df1aef6c3303782d26c12b908eb73125871072d
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-19 16:04:09 +0300

    case день недели

---
+++ Seminar1v3.1/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Write("Введите номер (от 1 • до • 7) дня недели:");
+28
+int numDay = Convert. ToInt32( Console. ReadLine ()
+29
+20
+switch (numDay)
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+43
+44
+45
+46
+48
+49
+SO
+case 1:
+Console.Write("Понедельник'
+break;
+case 2:
+Console.Write ("Вторник" break;
+raco 2•
+Console.write("Среда' break;
+case A•
+Console.Write ("Четверг' break;
+case 5:
+Console.Write("Пятница' break;
+case 6:
+Console.Write("Суббота* break;
+case 7:
+Console.Write("Воскресенье"
+break;
+52
+53
+default:
+Console.Write("Вы ввели не верный
+недели по заданному номеру
+Григорий Вахмистр...
+§ AUTUNE
+> TIMELINE
+0040
+In
