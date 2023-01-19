21e144134825e436217cc62f7e03f9d225232a88
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-19 16:22:32 +0300

    целые числа

--- Seminar1v4/Program.cs
+++ Seminar1v4/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите целое число ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+for(int i = -N; i <= N; i++)
+{
+    System.Console.Write(i + ", ");
+}
+
