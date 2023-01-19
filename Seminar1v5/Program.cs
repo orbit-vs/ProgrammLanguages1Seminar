069f8cd4125ac76f99e5426e034733e3c873c56b
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-19 16:23:03 +0300

    процент

---
+++ Seminar1v5/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine();
+
+System.Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.readLine());
+int result = number %10;
+System.Console.WriteLine(result);
